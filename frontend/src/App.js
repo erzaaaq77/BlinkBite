26b79960c438142107afb588c3e9cc7405a5e91e
AnisaLimani21 <al73645@ubt-uni.net>
2026-03-24 13:24:49 +0100

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Food Delivery Project</h1>
+      <button className="btn btn-success">Click me!</button>
+    </div>
+  );
+}
+
+export default App;
