eca3b933e02eef9bb2e57ff95de04ce27224635b
erzaaaq77 <eq73339@ubt-uni.net>
2026-03-25 10:20:35 +0100

    Database Migration

--- backend/FoodDeliveryyy/FoodDeliveryyy/Program.cs
+++ backend/FoodDeliveryyy/FoodDeliveryyy/Program.cs
@@ -1,3 +1,6 @@
+using FoodDeliveryyy;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = "Server=localhost;Database=FoodDelivery;User=root;Password=";
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 33)))
+);
 
 var app = builder.Build();
 // test backend git
@@ -21,5 +29,22 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+    // Krijon tabelat automatikisht në MySQL
+    db.Database.EnsureCreated();
+
+    // Shto një user për test
+    db.Users.Add(new User { Name = "Erza", Email = "erza@example.com" });
+    db.SaveChanges();
+
+    // Lexo dhe printo user-at
+    var users = db.Users.ToList();
+    foreach (var u in users)
+    {
+        Console.WriteLine($"User: {u.Name}, Email: {u.Email}");
+    }
+}
 app.Run();
---
+++ backend/FoodDeliveryyy/FoodDeliveryyy/User.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodDeliveryyy
+{
+
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
