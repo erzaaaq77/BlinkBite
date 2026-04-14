038dd8e47eef10beb4b8a8e4d75ae78379bc110e
erzaaaq77 <eq73339@ubt-uni.net>
2026-04-14 13:56:41 +0200

    push

---
+++ backend/FoodDeliveryyy/FoodDeliveryyy/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+
+namespace FoodDeliveryyy.Middleware
+{
+
+    public class GlobalExceptionHandlerMiddleware : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+        private readonly IWebHostEnvironment _env;
+
+
+        public GlobalExceptionHandler (ILogger<GlobalExceptionHandler> logger,IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+
+        }
+
+        public async ValueTask<bool>
+    }
+
+
+
+
+
+}
